241394b7588142ebf6cc0dda26a01b78d042666f
Duilio Palacios <i@duilio.me>
2016-05-05 12:35:15 +0100

    add missing view

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Users</div>
+
+                    <div class="panel-body" id="content">
+                        <table class="table">
+                        @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td><a href="#">Editar</a></td>
+                            </tr>
+                        @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
